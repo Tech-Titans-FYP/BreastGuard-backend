08d7615819cc61c3c9222c9463cffc52b218ee26
Malshi Kulasinghe <kulasinghe1malshi@gmail.com>
2024-02-01 15:43:56 +0530

    developed mri code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.ultrasound import ultrasound_image_modality
+from routes.mri import mri_image_modality
 
 app = Flask(__name__)
 CORS(app) # Allow CORS for all routes
@@ -9,6 +10,10 @@ app.config['UPLOAD_FOLDER'] = 'E:\\L4S1\\IS4910 - Comprehensive Group Project\\F
 @app.route('/api/process-us-image', methods=['POST'])
 def process_us_image():
     return ultrasound_image_modality()
+
+@app.route('/api/process-mri-image', methods=['POST'])
+def process_mri_image():
+    return mri_image_modality()
 
 if __name__ == '__main__':
     app.run(debug=True)
